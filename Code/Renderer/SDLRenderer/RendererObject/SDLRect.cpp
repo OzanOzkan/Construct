b94fb1d3b5ca3798bdb6fb951e975f27c6637ca1
OzanOzkan <ozanozkan@gmail.com>
2020-06-01 17:38:27 +0200

    Color support for Rect render object.

--- Code/Renderer/SDLRenderer/RendererObject/SDLRect.cpp
+++ Code/Renderer/SDLRenderer/RendererObject/SDLRect.cpp
@@ -13,17 +13,19 @@ void CSDLRect::Load(const SRenderObjectParams & params)
 	m_rotation	= params.rotation;
 	m_width		= params.width;
 	m_height	= params.height;
+	m_color		= params.color;
 }
 
 /////////////////////////////////////////////////
 void CSDLRect::RenderCopy()
 {
+	// TODO: Rotation.
 	SDL_Rect drawRect;
 	drawRect.x = m_position.x;
 	drawRect.y = m_position.y;
 	drawRect.w = m_width;
 	drawRect.h = m_height;
 
-	SDL_SetRenderDrawColor(m_pSDLRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	SDL_SetRenderDrawColor(m_pSDLRenderer, m_color.r, m_color.g, m_color.b, m_color.a);
 	SDL_RenderDrawRect(m_pSDLRenderer, &drawRect);
 }
